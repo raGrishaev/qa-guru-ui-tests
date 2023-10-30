99f2d6c5919696e5a2ff7bea8f9bd9ce3a06432d
Roman Grishaev <grishaev.r-a@ya.ru>
2023-10-30 04:49:38 +0300

    update test TestFormWithAllFields

--- src/test/java/tests/homework/TestFormWithAllFields.java
+++ src/test/java/tests/homework/TestFormWithAllFields.java
@@ -1,34 +1,27 @@
 package tests.homework;
 
-import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
 import helpers.Utils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import page.AutomationPracticeFormPageOblect;
 import page.enums.CityByUttarPradesh;
 import page.enums.Gender;
 import page.enums.Hobbies;
 import page.enums.Subjects;
-
-import java.io.File;
+import utils.TestBase;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TestFormWithAllFields {
+public class TestFormWithAllFields extends TestBase {
 
     AutomationPracticeFormPageOblect page = new AutomationPracticeFormPageOblect();
     Faker faker = new Faker();
     Utils utils = new Utils();
-    @BeforeAll
-    static void befocreAll(){
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-    }
 
     @Test
     void testFormWithAllFields(){
@@ -42,12 +35,13 @@ public class TestFormWithAllFields {
         String day = String.valueOf(RandomUtils.nextInt(0, 27));
         String firstSubject = utils.getRandomEnumValue(Subjects.class).toString();
         String hobie = utils.getRandomEnumValue(Hobbies.class).toString();
-        File image = new File("src/test/resources/2023-09-25_11h30_08.png");
         String currentAddress = faker.chuckNorris().fact();
-        String state = "Uttar Pradesh";//TODO: Подумать как можно выбирать рандомом штат, и с этого вариативно переходить в рандомный выбор города
+        String state = "Uttar Pradesh";
         String city = utils.getRandomEnumValue(CityByUttarPradesh.class).toString();
 
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 
         page.getInputFirstName().setValue(firstName);
         page.getInputLastName().setValue(lastName);
@@ -59,9 +53,8 @@ public class TestFormWithAllFields {
         page.getSelectorMonth().selectOption(month);
         page.getFieldDay().findBy(text(day)).click();
         page.getInputSubjects().setValue(firstSubject).pressEnter();
-        //page.getInputSubjects().setValue(secondSubject).pressEnter();
         page.getMultiBoxHobbies().findBy(text(hobie)).click();
-        page.getButtonUploadImage().uploadFile(image);
+        page.getButtonUploadImage().uploadFromClasspath("2023-09-25_11h30_08.png");
         page.getTextAreaCurrentAddress().setValue(currentAddress);
         page.getFieldState().click();
         page.getSelectorState().setValue(state).pressEnter();
@@ -70,14 +63,25 @@ public class TestFormWithAllFields {
 
         page.getButtonSubmit().click();
 
-        page.getTableResponsive().shouldHave(text(firstName + " " + lastName));
-        page.getTableResponsive().shouldHave(text(userEmail));
-        page.getTableResponsive().shouldHave(text(gender));
-        page.getTableResponsive().shouldHave(text(userNumber));
-        page.getTableResponsive().shouldHave(text(day + " " + month + "," + year));
-        page.getTableResponsive().shouldHave(text(firstSubject));
-        page.getTableResponsive().shouldHave(text(image.getName()));
-        page.getTableResponsive().shouldHave(text(currentAddress));
-        page.getTableResponsive().shouldHave(text(state + " " + city));
+        page.getTableResponsive().$(byText("Student Name")).parent()
+                .shouldHave(text(firstName + " " + lastName));
+        page.getTableResponsive().$(byText("Student Email")).parent()
+                .shouldHave(text(userEmail));
+        page.getTableResponsive().$(byText("Gender")).parent()
+                .shouldHave(text(gender));
+        page.getTableResponsive().$(byText("Mobile")).parent()
+                .shouldHave(text(userNumber));
+        page.getTableResponsive().$(byText("Date of Birth")).parent()
+                .shouldHave(text(day + " " + month + "," + year));
+        page.getTableResponsive().$(byText("Subjects")).parent()
+                .shouldHave(text(firstSubject));
+        page.getTableResponsive().$(byText("Hobbies")).parent()
+                .shouldHave(text(hobie));
+        page.getTableResponsive().$(byText("Picture")).parent()
+                .shouldHave(text("2023-09-25_11h30_08.png"));
+        page.getTableResponsive().$(byText("Address")).parent()
+                .shouldHave(text(currentAddress));
+        page.getTableResponsive().$(byText("State and City")).parent()
+                .shouldHave(text(state + " " + city));
     }
 }
---
+++ src/test/java/utils/TestBase.java
@@ -0,0 +1,15 @@
+package utils;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    static void setUpBrowser() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+    }
+
+}
