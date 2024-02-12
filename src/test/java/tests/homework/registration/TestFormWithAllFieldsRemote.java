f1854ab7563a745a6eec05b621db393b464aa2e8
Roman Grishaev <grishaev.r-a@ya.ru>
2024-02-12 14:55:42 +0300

    added text attach

--- src/test/java/tests/homework/registration/TestFormWithAllFieldsRemote.java
+++ src/test/java/tests/homework/registration/TestFormWithAllFieldsRemote.java
@@ -31,6 +31,8 @@ public class TestFormWithAllFieldsRemote {
     @AfterEach
     void addAttachments(){
         Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 
     Utils utils = new Utils();
